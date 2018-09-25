2993d3f3baff0ceee09793a24d8fc060e3a9b6e1
Rohit Farmer <rfarmer@swamicompute.wustl.edu>
2018-09-25 16:34:49 -0500

    Update

---
+++ extract_sdf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+'''
+Purpose: To extract a subset of mols from a long SDF file.
+extract_sdf.py -h for usage.
+'''
+
+# Standard Library.
+import argparse
+
+def extract_sdf(i, o, n):
+	'''
+	Extract a subset of mols from a long SDF file.
+	extract_sdf <input file name> <output file name> <number>
+	'''
+	count = 0
+
+	# Open an outfile.
+	outfile = open(o, 'w')
+
+	# Read and write n number of molecules from the input file.
+	with open(i, 'r') as file:
+		for line in file:
+			outfile.write(line)
+			if line.rstrip() == '$$$$':
+				count += 1
+			if count == n:
+				break
+
+	# Close the output file handle.
+	outfile.close()
+
+if __name__ == '__main__':
+	# Argument parser.
+	parser = argparse.ArgumentParser()
+	parser.add_argument("i", help="Input file name.")
+	parser.add_argument("o", help="Output file name.")
+	parser.add_argument("n", help="Number of molecules to return.", type=int)
+	args = parser.parse_args()
+
+	extract_sdf(args.i, args.o, args.n)
+
